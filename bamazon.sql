53c401eec209388a9da7e2544181a6df75281b97
Hari <augten95@gmail.com>
2019-08-30 16:46:13 -0500

    created a database called bamazeon; created a table called products and added 10 items

---
+++ bamazon.sql
@@ -0,0 +1,24 @@
+create database bamazon;
+use bamazon;
+create table products(
+		item_id int(4) not null,
+        product_name varchar(100) not null,
+        department_name varchar(100) not null,
+        price decimal(10,2) not null,
+        stock_quantity int(20) not null,
+        primary key(item_id)
+);
+
+select * from products;
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values (100, "tshirts", "clothing", 5.99, 20),
+		(200, "shoes", "footwear", 60.00, 18),
+        (300, "Microsoft Surface Pro 2", "tablets and computers", 200.00, 24),
+        (400, "socks", "footwear", 9.99, 32),
+        (500, "pucks", "hockey", 9.99, 19),
+        (600, "undershirts", "clothing", 4.99, 20),
+        (700, "UNO", "board and card games", 12.00, 10),
+        (800, "tealight candles", "home decor", 1.00, 100),
+        (900, "jeans", "clothing", 14.00, 40),
+        (1000, "Samsung Galaxy S10", "smartphones", 700.00, 80)
